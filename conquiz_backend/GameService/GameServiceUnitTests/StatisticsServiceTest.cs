da9975f9eafcf3cba36f6cdde3555a66d2b387c8
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2021-12-31 12:48:52 +0200

    Added statistics service tests

---
+++ conquiz_backend/GameService/GameServiceUnitTests/StatisticsServiceTest.cs
@@ -0,0 +1,108 @@
+﻿using GameService.Context;
+using GameService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+
+namespace GameServiceUnitTests
+{
+    public class StatisticsServiceTest
+    {
+        IDbContextFactory<DefaultContext> mockContextFactory;
+        IStatisticsService statisticsService;
+        Mock<IHttpContextAccessor> mockHttpContextAccessor;
+
+        public StatisticsServiceTest()
+        {
+            mockContextFactory = new TestDbContextFactory("StatServiceTest");
+            mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var httpContext = new DefaultHttpContext();
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+            };
+
+            mockHttpContextAccessor.Setup(h => h.HttpContext.User.Claims).Returns(claims);
+            statisticsService = new StatisticsService(mockContextFactory, mockHttpContextAccessor.Object);
+        }
+
+        [Fact]
+        public async Task GetPlayerStatisticsTest()
+        {
+            using var db = mockContextFactory.CreateDbContext();
+
+            // Arrange
+            var playerOne = new Users()
+            {
+                ExternalId = 123,
+                Username = "PlayerOne",
+            };
+            var playerTwo = new Users()
+            {
+                ExternalId = 152,
+                Username = "PlayerTwo",
+            };
+            var playerThree = new Users()
+            {
+                ExternalId = 181,
+                Username = "PlayerThree",
+            };
+            db.Add(playerOne);
+            db.Add(playerTwo);
+            db.Add(playerThree);
+            var allGames = new List<GameInstance>();
+            for (var i = 0; i < 4; i++)
+            {
+                var gm = new GameInstance()
+                {
+                    GameState = GameState.FINISHED,
+                };
+                gm.Participants.Add(new Participants()
+                {
+                    PlayerId = playerOne.Id,
+                    Score = 1500,
+                });
+                gm.Participants.Add(new Participants()
+                {
+                    PlayerId = playerTwo.Id,
+                    Score = 1000,
+                });
+                gm.Participants.Add(new Participants()
+                {
+                    PlayerId = playerThree.Id,
+                    Score = 500,
+                });
+
+                allGames.Add(gm);
+            }
+            db.AddRange(allGames);
+            await db.SaveChangesAsync();
+
+
+            // Second user trying to join lobby
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var httpContext = new DefaultHttpContext();
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, playerOne.Id.ToString()),
+            };
+
+            mockHttpContextAccessor.Setup(h => h.HttpContext.User.Claims).Returns(claims);
+
+            var secondaryUserService = new StatisticsService(mockContextFactory, mockHttpContextAccessor.Object);
+
+            var playerStats = await secondaryUserService.GetUserGameStatistics();
+            playerStats.GamesWon.ShouldBe(4);
+            playerStats.TotalGames.ShouldBe(4);
+            playerStats.WinPercentage.ShouldBe("100.00");
+
+        }
+    }
+}
