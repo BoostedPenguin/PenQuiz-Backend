33c6a2cf143f2cc7acb99e627e5cb659ab7cda13
BoostedPenguin <alex20009@abv.bg>
2022-01-07 09:47:01 +0100

    Added ping http call for question service

--- conquiz_backend/QuestionService/QuestionService/Controllers/QuestionController.cs
+++ conquiz_backend/QuestionService/QuestionService/Controllers/QuestionController.cs
@@ -20,6 +20,19 @@ namespace QuestionService.Controllers
             this.numberQuestionsService = numberQuestionsService;
         }
 
+        [HttpGet]
+        public IActionResult PingService()
+        {
+            try
+            {
+                return Ok("Successfully contacted ConQuiz question service. Version 1.0");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
         [HttpPost("number")]
         public async Task<IActionResult> CreateNumberQuestion([FromBody] CreateNumberQuestionRequest request)
         {
