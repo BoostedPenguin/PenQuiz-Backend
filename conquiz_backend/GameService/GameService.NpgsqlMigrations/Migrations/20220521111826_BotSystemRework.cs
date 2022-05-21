e3e27c56cb4d840d71175ac3dca7834d8370eca9
BoostedPenguin <alex20009@abv.bg>
2022-05-21 14:01:28 +0200

    Added bot migration

---
+++ conquiz_backend/GameService/GameService.NpgsqlMigrations/Migrations/20220521111826_BotSystemRework.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameService.NpgsqlMigrations.Migrations
+{
+    public partial class BotSystemRework : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isBot",
+                table: "Participants",
+                newName: "isAfk");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isBot",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isBot",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "isAfk",
+                table: "Participants",
+                newName: "isBot");
+        }
+    }
+}
