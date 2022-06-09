5d817516ad1626a6ae359fb2e5074d0efa1fb3b0
BoostedPenguin <alex20009@abv.bg>
2022-06-09 16:40:18 +0200

    Re-added previous character migrations

--- conquiz_backend/GameService/GameService.NpgsqlMigrations/Migrations/20220609143949_CharacterModelsAdded.Designer.cs
+++ conquiz_backend/GameService/GameService.NpgsqlMigrations/Migrations/20220609143949_CharacterModelsAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GameService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GameService.NpgsqlMigrations.Migrations
 {
     [DbContext(typeof(DefaultContext))]
-    partial class DefaultContextModelSnapshot : ModelSnapshot
+    [Migration("20220609143949_CharacterModelsAdded")]
+    partial class CharacterModelsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
