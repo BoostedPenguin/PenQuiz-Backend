4fb1c9ade3cf580d358382572c191e49f6aad3ec
BoostedPenguin <alex20009@abv.bg>
2022-07-02 17:55:49 +0200

    Re-added proper user character migration

---
+++ conquiz_backend/GameService/GameService.NpgsqlMigrations/Migrations/20220702155437_CharacterUsedByUser.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameService.NpgsqlMigrations.Migrations
+{
+    public partial class CharacterUsedByUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CharacterUsers",
+                columns: table => new
+                {
+                    BelongToUsersId = table.Column<int>(type: "integer", nullable: false),
+                    OwnedCharactersId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterUsers", x => new { x.BelongToUsersId, x.OwnedCharactersId });
+                    table.ForeignKey(
+                        name: "FK_CharacterUsers_Characters_OwnedCharactersId",
+                        column: x => x.OwnedCharactersId,
+                        principalTable: "Characters",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CharacterUsers_Users_BelongToUsersId",
+                        column: x => x.BelongToUsersId,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterUsers_OwnedCharactersId",
+                table: "CharacterUsers",
+                column: "OwnedCharactersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterUsers");
+        }
+    }
+}
